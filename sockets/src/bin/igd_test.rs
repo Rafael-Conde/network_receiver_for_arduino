191da323a883b15b97c4cbbee33e549efd86b81c
Rafael de Conde <GitHubRafael@gmail.com>
2022-08-31 16:02:07 -0300

    file with a likely working UPnP executable

---
+++ sockets/src/bin/igd_test.rs
@@ -0,0 +1,55 @@
+use std::{net::{Ipv4Addr, SocketAddrV4, TcpListener, SocketAddr, IpAddr}, io::Read};
+
+extern crate igd;
+
+fn main()
+{
+	match igd::search_gateway(Default::default())
+	{
+		Err(ref err) => println!("Error: {}", err),
+		Ok(gateway) =>
+		{
+			let local_addr = match std::env::args().nth(1)
+			{
+				Some(local_addr) => local_addr,
+				None => panic!("Expected IP address (cargo run --example add_port <your IP here>)"),
+			};
+			let local_addr = local_addr.parse::<Ipv4Addr>().unwrap();
+			let local_addr = SocketAddrV4::new(local_addr, 50000u16);
+
+			match gateway.add_port(
+			                       igd::PortMappingProtocol::TCP,
+			                       50000,
+			                       local_addr,
+			                       60,
+			                       "add_port example",
+			)
+			{
+				Err(ref err) =>
+				{
+					println!("There was an error! {}", err);
+				}
+				Ok(()) =>
+				{
+					println!("It worked");
+					let loopback = "192.168.1.102".trim() //"::".trim()
+					                              .parse::<IpAddr>()
+					                              .expect("Error while parsing the address");
+					let port: u16 = 50000;
+					let socket = SocketAddr::new(loopback, port);
+					let listener = TcpListener::bind(socket).unwrap();
+					println!(
+					         "Listening on {}, access this port to end the program",
+					         socket
+					);
+
+					let (mut tcp_stream, addr) = listener.accept().unwrap(); //block  until requested
+					println!("Connection received! {:?} is sending data.", addr);
+					let mut input = String::new();
+					let _ = tcp_stream.read_to_string(&mut input).unwrap();
+					println!("{:?} says {}", addr, input);
+				}
+			}
+		}
+	}
+}
