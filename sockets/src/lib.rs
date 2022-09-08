f583761d9a490a3dd9f6dcc6fe463668c4f5fc6f
Rafael de Conde <GitHubRafael@gmail.com>
2022-09-08 18:26:18 -0300

    The implementation should be read to use, but isn't working currently. saving as a reference

--- sockets/src/lib.rs
+++ sockets/src/lib.rs
@@ -5,67 +5,62 @@
         clippy::unwrap_used,
         clippy::expect_used)]
 
-use std::net::{SocketAddrV4, TcpListener};
 pub use igd::PortMappingProtocol;
-
-/// todo docs for the network_protocol enum
-///
+use std::net::{SocketAddrV4, TcpListener};
 
 pub mod networking
 {
 	use std::net::IpAddr;
 
-        use super::*;
+	use super::*;
 
+	pub fn open_server(protocol: igd::PortMappingProtocol, port: u16)
+	                   -> Result<TcpListener, String>
+	{
+		let gateway = match igd::search_gateway(Default::default())
+		{
+			Err(e) => return Err(e.to_string()),
+			Ok(gateway) => gateway,
+		};
+		let local_addr = match local_ip_address::local_ip()
+		{
+			Err(e) => return Err(e.to_string()),
+			Ok(local_addr) =>
+			{
+				if let IpAddr::V4(ipv4) = local_addr
+				{
+					ipv4
+				}
+				else
+				{
+					return Err(String::from("The local IP address isn't the IPv4."));
+				}
+			}
+		};
+		println!("The local ip is: {}", local_addr);
 
-	pub fn open_server(protocol: igd::PortMappingProtocol,
-	                   port: u16)
-	        -> Result<TcpListener, String>
-	        {
-		        let gateway = match igd::search_gateway(igd::SearchOptions::default())
-		        {
-		                Err(e) => return Err(e.to_string()),
-		                Ok(gateway) => gateway
+		//add logging for which port is
+		//being used
+		let local_addr = SocketAddrV4::new(local_addr, port); //add a way to get the
+													  //local ip address
 
-		        };
-		        let local_addr = match local_ip_address::local_ip()
-		        {
-		                Err(e) => return Err(e.to_string()),
-		                Ok(local_addr) => 
-		                        if let IpAddr::V4(ipv4) = local_addr
-		                        {
-                                                ipv4
-		                        }
-		                        else
-		                        {
-                                                return Err(String::from("The local IP address isn't the IPv4."));
-		                        }
-		        };
-
-		        //add logging for which port is
-		        //being used
-	                let local_addr = SocketAddrV4::new(local_addr, port); //add a way to get the
-							                      //local ip address
-
-	                if let Err(e) = gateway.add_port(
-		                protocol,
-		                port,
-		                local_addr,
-		                20,
-		                "Server to transmite to arduino the incoming data from google colab",
-		                )
-	                {
-		                return Err(e.to_string());
-	                };
-	                //include logging for successfull addewd port on the router
-                        match TcpListener::bind(local_addr)
-	                {
-		                Err(e) => Err(e.to_string()),
-		                Ok(TcpListener) => //log that the server started
-		                        Ok(TcpListener)
-
-	                }
-	        }
+		if let Err(e) =
+			gateway.add_port(
+			                 protocol,
+			                 port,
+			                 local_addr,
+			                 20,
+			                 "Server to transmite to arduino the incoming data from google colab",
+			)
+		{
+			return Err(e.to_string());
+		};
+		//include logging for successfull addewd port on the router
+		match TcpListener::bind(local_addr)
+		{
+			Err(e) => Err(e.to_string()),
+			Ok(TcpListener) => Ok(TcpListener), //log that the server started0
+		}
+	}
 	//add a logging that the server started
 }
-
--- sockets/src/main.rs
+++ sockets/src/main.rs
@@ -6,24 +6,33 @@
 //         clippy::expect_used)]
 //
 
+use std::io::Read;
 use std::process;
 
-use sockets::{networking,PortMappingProtocol};
+use sockets::{networking, PortMappingProtocol};
 
 fn main()
 {
-    let listener = networking::open_server(PortMappingProtocol::TCP,50000u16).unwrap();
-    let (mut tcp_stream, _) = match listener.accept()
-    {
-        Err(e) => panic!("{e}"),
-        Ok((mut tcp_stream, addr)) => (tcp_stream, addr),
-    };
-    let mut input_buff = String::new();
-    if let Err(e) = tcp_stream.read_to_string(&mut input_buff)
+	let listener = match networking::open_server(PortMappingProtocol::TCP, 50000u16)
 	{
-        println!("{e}");
-	    process::exit(1);
-	};
-    println!("{}", input_buff);
+		Err(t) =>
+		{
+			println!("{}", t);
+			panic!();
+		}
+		Ok(tcp) => tcp,
 
+	};
+	let (mut tcp_stream, _) = match listener.accept()
+	{
+		Err(e) => panic!("{e}"),
+		Ok((tcp_stream, addr)) => (tcp_stream, addr),
+	};
+	let mut input_buff = String::new();
+	if let Err(e) = tcp_stream.read_to_string(&mut input_buff)
+	{
+		println!("{e}");
+		process::exit(1);
+	};
+	println!("{}", input_buff);
 }
