bfadd812e23145e3931f54ecdefe6f91c8858b72
Rafael de Conde <GitHubRafael@gmail.com>
2022-09-08 14:27:20 -0300

    started refactoring the code, added compiler warnnings and separeted module for the library functions

--- sockets/src/bin/igd_test.rs
+++ sockets/src/bin/igd_test.rs
@@ -1,10 +1,21 @@
-use std::{net::{Ipv4Addr, SocketAddrV4, TcpListener, SocketAddr, IpAddr}, io::Read};
+// #![warn(clippy::all,
+//         clippy::pedantic,
+//         clippy::nursery,
+//         clippy::cargo,
+//         clippy::unwrap_used)]
+//
+use std::{
+	io::Read,
+	net::{Ipv4Addr, SocketAddrV4, TcpListener},
+};
 
 extern crate igd;
 
 fn main()
 {
-	match igd::search_gateway(Default::default())
+	// testando a edição de texto com Astro NeoVim
+	// This is incredible!!!
+	match igd::search_gateway(igd::SearchOptions::default())
 	{
 		Err(ref err) => println!("Error while Searching gateway: {}", err),
 		Ok(gateway) =>
@@ -17,39 +28,36 @@ fn main()
 			let local_addr = local_addr.parse::<Ipv4Addr>().unwrap();
 			let local_addr = SocketAddrV4::new(local_addr, 50000u16);
 
-			match gateway.add_port(
-			                       igd::PortMappingProtocol::TCP,
-			                       50000,
-			                       local_addr,
-			                       20,
-			                       "add_port example",
+			if let Err(ref err) = gateway.add_port(
+			                                       igd::PortMappingProtocol::TCP,
+			                                       50000,
+			                                       local_addr,
+			                                       20,
+			                                       "add_port example",
 			)
 			{
-				Err(ref err) =>
-				{
-					println!("There was an error! {}", err);
-				}
-				Ok(()) =>
-				{
-					println!("It worked");
-					// let loopback = "192.168.1.102".trim() //"::".trim()
-					//                               .parse::<IpAddr>()
-					//                               .expect("Error while parsing the address");
-					// let port: u16 = 50000;
-					// let socket = SocketAddr::new(local_addr, port);
-                    // let socket = SocketAddr::new("192.168.15.10".parse::<IpAddr>().unwrap(), port);
-					let listener = TcpListener::bind(local_addr).unwrap();
-					println!(
-					         "Listening on {}, access this port to end the program",
-					         local_addr
-					);
+				println!("There was an error! {}", err);
+			}
+			else
+			{
+				println!("It worked");
+				// let loopback = "192.168.1.102".trim() //"::".trim()
+				//                               .parse::<IpAddr>()
+				//                               .expect("Error while parsing the address");
+				// let port: u16 = 50000;
+				// let socket = SocketAddr::new(local_addr, port);
+				// let socket = SocketAddr::new("192.168.15.10".parse::<IpAddr>().unwrap(), port);
+				let listener = TcpListener::bind(local_addr).unwrap();
+				println!(
+				         "Listening on {}, access this port to end the program",
+				         local_addr
+				);
 
-					let (mut tcp_stream, addr) = listener.accept().unwrap(); //block  until requested
-					println!("Connection received! {:?} is sending data.", addr);
-					let mut input = String::new();
-					let _ = tcp_stream.read_to_string(&mut input).unwrap();
-					println!("{:?} says {}", addr, input);
-				}
+				let (mut tcp_stream, addr) = listener.accept().unwrap(); //block  until requested
+				println!("Connection received! {:?} is sending data.", addr);
+				let mut input = String::new();
+				let _ = tcp_stream.read_to_string(&mut input).unwrap();
+				println!("{:?} says {}", addr, input);
 			}
 		}
 	}
--- sockets/src/main.rs
+++ sockets/src/main.rs
@@ -1,30 +1,29 @@
-use std::io::{stdin, Error, Read};
-use std::net::{IpAddr, SocketAddr, TcpListener};
+// #![warn(clippy::all,
+//         clippy::pedantic,
+//         clippy::nursery,
+//         clippy::cargo,
+//         clippy::unwrap_used,
+//         clippy::expect_used)]
+//
 
-fn main() -> Result<(), Error>
+use std::process;
+
+use sockets::{networking,PortMappingProtocol};
+
+fn main()
 {
-	println!("Please insert your IP address:");
-	let mut input_string = String::new();
-	stdin().read_line(&mut input_string)
-	       .expect("Failed to read line");
+    let listener = networking::open_server(PortMappingProtocol::TCP,50000u16).unwrap();
+    let (mut tcp_stream, _) = match listener.accept()
+    {
+        Err(e) => panic!("{e}"),
+        Ok((mut tcp_stream, addr)) => (tcp_stream, addr),
+    };
+    let mut input_buff = String::new();
+    if let Err(e) = tcp_stream.read_to_string(&mut input_buff)
+	{
+        println!("{e}");
+	    process::exit(1);
+	};
+    println!("{}", input_buff);
 
-	let loopback = input_string.trim()
-	                           .parse::<IpAddr>()
-	                           .expect("Error while parsing the address");
-	let port: u16 = 50000;
-	let socket = SocketAddr::new(loopback, port);
-
-	let listener = TcpListener::bind(socket)?;
-	println!(
-	         "Listening on {}, access this port to end the program",
-	         socket
-	);
-
-	let (mut tcp_stream, addr) = listener.accept()?; //block  until requested
-	println!("Connection received! {:?} is sending data.", addr);
-	let mut input = String::new();
-	let _ = tcp_stream.read_to_string(&mut input)?;
-	println!("{:?} says {}", addr, input);
-
-	Ok(())
 }
